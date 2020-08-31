2470577663561ce33046b9587551bcf73bc30bdf
n3oney <neoney@pm.me>
2020-08-31 18:10:48 +0200

    👌 IMPROVE: remove an unused parameter

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.use(cookieSession({
 
 app.use("/api/auth", Auth);
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     res.json({
         message: "Welcome to httpreq.club"
     });
