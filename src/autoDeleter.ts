1ae6b4f67f9fe248c0e2f3449b814b505b3811d5
n3oney <neoney@pm.me>
2020-09-04 20:20:16 +0200

    🐛 FIX: Autodeleter function

--- src/autoDeleter.ts
+++ src/autoDeleter.ts
@@ -1,9 +1,9 @@
-import Global from "./global";
+import Global from "./Global";
 
 export async function autoDelete() {
     const files = await Global.db.collection("files").find({ expiry:  { $lt: Date.now() } }).toArray();
 
-    for(let file in files) {
+    for(let file of files) {
         await Global.db.collection("files").deleteOne({_id: file._id});
     }
 }
