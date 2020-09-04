41b1b0f8eecdfeb64078e4d53cafb03cc1487859
n3oney <neoney@pm.me>
2020-09-04 20:23:36 +0200

    🐛 FIX: Make autodeleter delete the files from Mongo

--- src/autoDeleter.ts
+++ src/autoDeleter.ts
@@ -1,9 +1,12 @@
 import Global from "./Global";
+import { GridFSBucket } from "mongodb";
 
 export async function autoDelete() {
     const files = await Global.db.collection("files").find({ expiry:  { $lt: Date.now() } }).toArray();
 
     for(let file of files) {
         await Global.db.collection("files").deleteOne({_id: file._id});
+        const bucket = new GridFSBucket(Global.db);
+        bucket.delete(file._id);
     }
 }
