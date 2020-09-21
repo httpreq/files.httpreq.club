2ebe56a47d43e6038c22e23fff50d197df44f97e
Loron gröbner <groebner.loron@gmail.com>
2020-09-21 23:36:20 +0200

    ✨ Added filter

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import "dotenv/config";
-import argon from "argon2";
-import express from "express";
-import Logger from "./Logger";
-import helmet from "helmet";
-import cors from "cors";
-import multer from "multer";
-import Global from "./Global";
-import Auth from "./Routers/Auth";
-import bodyParser from "body-parser";
-import cookieSession from "cookie-session";
-import { GridFSBucket } from "mongodb";
-import {autoDelete} from "./autoDeleter";
+import 'dotenv/config';
+
+import argon from 'argon2';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import {GridFSBucket} from 'mongodb';
+import multer from 'multer';
+
+import {autoDelete} from './autoDeleter';
+import Global from './Global';
+import Logger from './Logger';
+import Auth from './Routers/Auth';
 
 setInterval(autoDelete, 60000);
 
@@ -18,96 +20,103 @@ const upload = multer();
 
 const app = express();
 
-app.set("trust proxy", 1);
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
-    name: "ses",
-    keys: [process.env.SESSION_KEY1!, process.env.SESSION_KEY2!],
-    maxAge: 86400000 /* 1 day */
+  name: 'ses',
+  keys: [process.env.SESSION_KEY1!, process.env.SESSION_KEY2!],
+  maxAge: 86400000 /* 1 day */
 }));
 
-app.use("/api/auth", Auth);
+app.use('/api/auth', Auth);
 
-app.get("/", (_req, res) => {
-    res.json({
-        message: "Welcome to httpreq.club"
-    });
+app.get('/', (_req, res) => {
+  res.json({message: 'Welcome to httpreq.club'});
 });
 
-app.post("/api/upload", upload.single("file"), async (req, res) => {
-    if (!req.file)
-        return res.status(400).json({ message: "No file to upload" });
+async function sendError(req: any, content: string) {
+  return req.status(200).json({message: content});
+}
 
-    let userId: number | null = null;
-    if (req.session!.hasOwnProperty("userId"))
-        userId = Number(req.session!.userId);
-    else {
-        if (!req.headers.hasOwnProperty("authorization")) return res.status(401).json({ message: "No way to authorize user" });
-        const users = await Global.db.collection("users").find().toArray();
-        for (let user of users) {
-            if (argon.verify(user.hash, req.headers.authorization!)) {
-                userId = user._id;
-                break;
-            }
-        }
+app.post('/api/upload', upload.single('file'), async (req, res) => {
+  if (!req.file) return res.status(400).json({message: 'No file to upload'});
+
+  let userId: number|null = null;
+  if (req.session!.hasOwnProperty('userId'))
+    userId = Number(req.session!.userId);
+  else {
+    if (!req.headers.hasOwnProperty('authorization'))
+      return res.status(401).json({message: 'No way to authorize user'});
+    const users = await Global.db.collection('users').find().toArray();
+    for (let user of users) {
+      if (argon.verify(user.hash, req.headers.authorization!)) {
+        userId = user._id;
+        break;
+      }
     }
+  }
 
-    if (userId === null) return res.status(403).json({ message: "Incorrect token" });
+  if (userId === null)
+    return res.status(403).json({message: 'Incorrect token'});
 
-    const bucket = new GridFSBucket(Global.db);
+  const bucket = new GridFSBucket(Global.db);
 
-    const id = String(await Global.getNextSequenceValue("fileid"));
+  const id = String(await Global.getNextSequenceValue('fileid'));
 
-    const uploadStream = bucket.openUploadStream(id);
-    const stream = req.file.stream.pipe(uploadStream);
+  const uploadStream = bucket.openUploadStream(id);
+  const stream = req.file.stream.pipe(uploadStream);
 
-    // Credits to the retention maths : 0x0.st
+  // Credits to the retention maths : 0x0.st
 
-    const minAge = 7; // Days
-    const maxAge = 30; // Days
-    const maxSize = 100; // Megabytes
-    const fileSize = req.file.size / 1048576;
-    console.log(fileSize);
-    const retention = minAge + (-maxAge + minAge) * Math.pow((fileSize / maxSize - 1), 3);
-    const time_to_live = Date.now() + (retention * 86400000);
+  const minAge = 7;     // Days
+  const maxAge = 30;    // Days
+  const maxSize = 100;  // Megabytes
+  const fileSize = req.file.size / 1048576;
+  console.log(fileSize);
+  const retention =
+      minAge + (-maxAge + minAge) * Math.pow((fileSize / maxSize - 1), 3);
+  const time_to_live = Date.now() + (retention * 86400000);
 
-    stream.on("finish", async () => {
-        await Global.db.collection("files").insertOne({
-            _id: id,
-            userId: userId,
-            fileId: uploadStream.id,
-            mimeType: (req.file as any).detectedMimeType,
-            expiry: time_to_live,
-        });
-        res.status(201).json({
-            address: "/" + id
-        });
+  const type = (req.file as any).detectedMimeType;
+
+  switch(type) {
+    case "application":
+      sendError(req, "The file type provided is not allowed");
+      break;
+
+      /* add more here */
+      
+    default: 
+      /* should upload */
+      break;
+  }
+
+  stream.on('finish', async () => {
+    await Global.db.collection('files').insertOne({
+      _id: id,
+      userId: userId,
+      fileId: uploadStream.id,
+      mimeType: type,
+      expiry: time_to_live,
     });
+    res.status(201).json({address: '/' + id});
+  });
 });
 
-app.get("/:id", async (req, res) => {
-    const file = await Global.db.collection("files").findOne({
-        _id: req.params.id
-    }, {
-        projection: {
-            _id: 0,
-            mimeType: 1
-        }
-    });
-if (!file) return res.status(404).end();
+app.get('/:id', async (req, res) => {
+  const file = await Global.db.collection('files').findOne(
+      {_id: req.params.id}, {projection: {_id: 0, mimeType: 1}});
+  if (!file) return res.status(404).end();
 
-    res.contentType(file.mimeType as string);
-    const bucket = new GridFSBucket(Global.db);
-    const stream = bucket.openDownloadStreamByName(req.params.id);
-    stream.pipe(res);
+  res.contentType(file.mimeType as string);
+  const bucket = new GridFSBucket(Global.db);
+  const stream = bucket.openDownloadStreamByName(req.params.id);
+  stream.pipe(res);
 });
 
 app.listen(process.env.PORT || 4042, () => {
-    Logger.success(`Listening on localhost:${process.env.PORT || 4042}`);
+  Logger.success(`Listening on localhost:${process.env.PORT || 4042}`);
 });
